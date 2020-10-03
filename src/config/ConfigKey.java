80aa95e376d657c2425596843983a4fc1145614f
Lavigne <mat.live@live.fr>
2020-10-03 12:22:16 +0200

    Gestion de la barre espace (pour les machines)

--- src/config/ConfigKey.java
+++ src/config/ConfigKey.java
@@ -21,9 +21,7 @@ public enum ConfigKey {
 	TOUCHE_BAS("ToucheBas"),
 	TOUCHE_DROITE("ToucheDroite"),
 	TOUCHE_GAUCHE("ToucheGauche"),
-	TOUCHE_POSER_MUR("TouchePoserMur"), 
-	MAP_DIRECTORY("MapsDirectory"),
-	TOUCHE_MINER("ToucheMiner");
+	TOUCHE_MACHINE("ToucheActiveMachine");
 	
 	
 	private final static Map<String, String> noms = new HashMap<>();
