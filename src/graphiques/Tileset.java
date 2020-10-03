8193ff30a32909e749d2a7668d196a4b55bf27c2
Adrien Duval <adrien.duval.bonnet@gmail.com>
2020-10-04 00:24:54 +0200

    PLEIN DE BORDEL DSL DSL DSL

--- src/graphiques/Tileset.java
+++ src/graphiques/Tileset.java
@@ -10,19 +10,27 @@ public class Tileset {
 	private List<PImage> images;
 
 	public Tileset(PImage img, int nbX, int nbY) {
-		w = nbX;
-		h = nbY;
-		wTile = img.width / w;
-		hTile = img.height / h;
-				
-		images = new ArrayList<>();
-		for (int y = 0; y < nbY; y++) 
-			for (int x = 0; x < nbX; x++)
-				images.add(img.get(x * wTile, y * hTile, wTile, hTile));
+		this(img, nbX, nbY, 0, 0);
 	}
 	
 	public Tileset(String imageName, int nbX, int nbY) {
 		this(Assets.getImage(imageName), nbX, nbY);
+	}
+	
+	public Tileset(PImage img, int nbX, int nbY, int decalageX, int decalageY) {
+		w = nbX;
+		h = nbY;
+		wTile = (img.width + 1) / w - decalageX;
+		hTile = (img.height + 1) / h - decalageY;
+				
+		images = new ArrayList<>();
+		for (int y = 0; y < nbY; y++) 
+			for (int x = 0; x < nbX; x++)
+				images.add(img.get(x * (wTile + decalageX), y * (hTile + decalageY), wTile - decalageX, hTile - decalageY));
+	}
+	
+	public Tileset(String imageName, int nbX, int nbY, int decalageX, int decalageY) {
+		this(Assets.getImage(imageName), nbX, nbY, decalageX, decalageY);
 	}
 
 	public PImage get(int index) {		
