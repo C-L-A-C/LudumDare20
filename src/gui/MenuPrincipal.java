91c73374cb3e687fc119ce34d711ab61c8d1bc65
Nicolas Bon <nicolas.bon38@hotmail.fr>
2020-10-05 19:21:20 +0200

    new niveau

--- src/gui/MenuPrincipal.java
+++ src/gui/MenuPrincipal.java
@@ -163,7 +163,7 @@ public class MenuPrincipal extends Scene {
 		super.mousePressed();
 		if (buttonCampaign.contient(p.mouseX, p.mouseY)) {
 			p.cursor(p.ARROW);
-			SceneHandler.setRunning(new Jeu(4));
+			SceneHandler.setRunning(new Jeu(6));
 		}
 		if (buttonCredits.contient(p.mouseX, p.mouseY)) {
 			SceneHandler.setRunning(new EcranTuto(p));
